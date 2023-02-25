f6a7b066dafb7b1692f60df33b8f3e5b7fa20e2e
Arman <armanmail@gmail.com>
2023-02-25 19:42:08 +0400

    commit22

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,11 +1,17 @@
 #include "push_swap.h"
+#include <stdio.h>
 
 size_t	ft_strlen(char *str)
 {
 	size_t	a;
+	size_t	len;
 
 	a = 0;
-	while (str[a] != '\0')
+	len = 0;
+	while (str[a] != '\0' && str[a] == '0')
 		a++;
-	return (a);
+	len = a;
+	while(str[len] != '\0')
+		len++;
+	return (len - a);
 }
